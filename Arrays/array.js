c78f0ad5814307904303d0ec56945b2192f2d379
ganesh2425 <saahoganesh023@gmail.com>
2021-11-02 09:20:52 +0530

    pl

---
+++ Arrays/array.js
@@ -0,0 +1,5 @@
+const salad = ["kivi","guava","cherry","tomato"];
+
+for(let i=0; i<salad.length; i++) {
+    console.log(`Element at index ${i} is ${salad[i]}`);
+  }
---
+++ Javascript/one.js
@@ -0,0 +1,2 @@
+let array = [10,6,3,7,19,45,8]
+console.log (array.sort((a,b)=>{return a>b}))
